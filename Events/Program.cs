b5e8599d201a4c75ff636b43a920ee21367cf090
Maxim Elushev <maxim.elushev@yandex.ru>
2022-03-22 00:25:25 +0300

    FirstTask

---
+++ Events/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace Events
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var key = new KeyTest();
+            key.OnKeyPressed += (Sender, PressKey) => Console.WriteLine($"\nВведённый символ: {PressKey}");
+            key.Run();
+
+        }
+    }
+
+    internal class KeyTest
+    {
+        public event EventHandler<char> OnKeyPressed;
+
+        public void Run()
+        {
+            while (true)
+            {
+                Console.Write("Введите символ: ");
+                var symbol = Console.ReadKey();
+                if (symbol.Key == ConsoleKey.C)
+                {
+                    break;
+                }
+                else
+                {
+                    OnKeyPressed?.Invoke(this, symbol.KeyChar);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
